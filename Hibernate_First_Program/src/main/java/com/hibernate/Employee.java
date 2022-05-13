757941ff961ddbdeba936ec868c3900681fc9929
yaminikeerthivadlapatla <99263048+yaminikeerthivadlapatla@users.noreply.github.com>
2022-05-13 12:52:20 +0530

    Add files via upload
    commit

---
+++ Hibernate_First_Program/src/main/java/com/hibernate/Employee.java
@@ -0,0 +1,41 @@
+package com.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
+public class Employee {
+	
+	@Id
+	@Column(name="e_id")
+	int id;
+	@Column(name="e_name")
+	String name;
+	@Column(name="e_city")
+	String city;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+
+}
